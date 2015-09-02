806ef64c4d03fcc4965ac12f187550fae2b8fc1e
Will Sutherland <wgpsutherland@gmail.com>
2015-09-02 23:46:45 +0100

    Create main shell that for now initialises a game

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
+//==================== NODE MODULES =====================//
+
 require('babel/register');
+
+//==================== LOCAL INCLUDES ===================//
+
+var Game = require('./game');
+var Player = require('./player');
+
+//==================== COMPONENTS =======================//
+
+var redPlayer = new Player();
+var bluePlayer = new Player();
+
+var connect4Game = new Game(redPlayer, bluePlayer);
+
+//==================== RUN ==============================//

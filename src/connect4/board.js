0fb07de79d75be73c2b100a741a5a1654dbe0025
wgpsutherland <wgpsutherland@gmail.com>
2015-09-03 15:52:26 +0100

    Create play function in the connect 4 board
    Add underscore

--- src/connect4/board.js
+++ src/connect4/board.js
@@ -1,3 +1,5 @@
+const _ = require('underscore');
+
 const Slot = require('./slot');
 const Emoji = require('./emoji');
 
@@ -14,15 +16,24 @@ class Board {
         return this.slots[row][col];
     }
 
+    play(col, colour) {
+        // finds the first empty slot in the column from the bottom up
+        let slot = _.find(this.slots, (row) => {
+            return row[col].empty;
+        })[col];
+        slot.play(colour);
+        this.lastPlayedSlot = slot;
+    }
+
     toString() {
-        var display = "";
+        let display = "";
         for (let i = this.slots.length - 1; i >= 0; i--) { // so board prints in the correct orientation
             for (let slot of this.slots[i]) {
                 display += slot.toString();
             }
             display += '\n';
         }
-        for(let i = 1; i<= 7; i++) {
+        for (let i = 1; i <= 7; i++) {
             display += Emoji.number[i] + ' ';
         }
         return display;
@@ -30,7 +41,7 @@ class Board {
 }
 
 function initialiseSlots() {
-    var rows = [];
+    let rows = [];
     for (let i = 0; i < HEIGHT; i++) {
         let columns = [];
         for (let j = 0; j < WIDTH; j++) {

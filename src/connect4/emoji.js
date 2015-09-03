24c1e501a1316edcd8fedcdf8c55a46f15eef34b
Will Sutherland <wgpsutherland@gmail.com>
2015-09-03 21:57:49 +0100

    Add extra modifier to the number emojis & improve code slightly

--- src/connect4/emoji.js
+++ src/connect4/emoji.js
@@ -1,3 +1,5 @@
+const numModifier = '\uFE0F\u20E3';
+
 module.exports = {
     circle: {
         red: '\u{1F534}',
@@ -5,15 +7,15 @@ module.exports = {
         white: '\u{26AA}'
     },
     number: {
-        0: '\u0030\u20E3',
-        1: '\u0031\u20E3',
-        2: '\u0032\u20E3',
-        3: '\u0033\u20E3',
-        4: '\u0034\u20E3',
-        5: '\u0035\u20E3',
-        6: '\u0036\u20E3',
-        7: '\u0037\u20E3',
-        8: '\u0038\u20E3',
-        9: '\u0039\u20E3'
+        0: '\u0030' + numModifier,
+        1: '\u0031' + numModifier,
+        2: '\u0032' + numModifier,
+        3: '\u0033' + numModifier,
+        4: '\u0034' + numModifier,
+        5: '\u0035' + numModifier,
+        6: '\u0036' + numModifier,
+        7: '\u0037' + numModifier,
+        8: '\u0038' + numModifier,
+        9: '\u0039' + numModifier
     }
 };

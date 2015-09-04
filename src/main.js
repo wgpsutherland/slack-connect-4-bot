a30f7ddb3c1cbfe14796021d65e6d6de68d85740
wgpsutherland <wgpsutherland@gmail.com>
2015-09-04 16:11:15 +0100

    Create server and token listener

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 //==================== NODE MODULES =====================//
 
 require('babel/register');
+const http = require('http');
 
 //==================== LOCAL INCLUDES ===================//
 
 const Bot = require('./bot/bot');
 
-//==================== COMPONENTS =======================//
-
-const bot = new Bot('hello');
-
 //==================== RUN ==============================//
 
+try {
+    const fs = require('fs');
+    const pathToken = process.env.SLACK_CONNECT_4_BOT_TOKEN;
+    const token = pathToken || fs.readFileSync('token.txt', 'utf-8').trim();
+} catch(error) {
+    console.log('You need to put your API token in a token.txt file');
+    return;
+}
+
+const bot = new Bot(token);
 bot.login();
+
+http.createServer(function(req, res) {
+    res.end('SLACK_CONNECT_4_BOT');
+}).listen(process.env.port || 5000);

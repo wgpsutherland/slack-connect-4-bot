bb1cc2e6a29080e86b6a424c4eaaaabcdbc339d9
Will Sutherland <wgpsutherland@gmail.com>
2016-12-14 23:19:20 +0000

    Fix token error in main file

--- src/main.js
+++ src/main.js
@@ -5,15 +5,16 @@ try {
     const fs = require('fs');
     const pathToken = process.env.SLACK_CONNECT_4_BOT_TOKEN;
     const token = pathToken || fs.readFileSync('token.txt', 'utf-8').trim();
+    const Bot = require('./bot/bot');
+    const bot = new Bot(token);
+    
+    bot.login();
+
+    http.createServer(function(req, res) {
+        res.end('SLACK_CONNECT_4_BOT');
+    }).listen(process.env.PORT || 5000);
+
 } catch(error) {
     console.log('You need to put your API token in a token.txt file');
     return;
 }
-
-const Bot = require('./bot/bot');
-const bot = new Bot(token);
-bot.login();
-
-http.createServer(function(req, res) {
-    res.end('SLACK_CONNECT_4_BOT');
-}).listen(process.env.PORT || 5000);
